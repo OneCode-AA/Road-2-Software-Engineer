cc57180bf50e5c6224fb393698a07b4f490b348c
abooabdillaah.mbj <onecode.aa@gmail.com>
2024-08-17 18:12:07 -0500

    update

---
+++ 4. JavaScript (Stack Two)/Fundamentals/Variables, Types and Expressions/variables/main.js
@@ -0,0 +1,13 @@
+// Variables
+
+// Variable names follow these rules:
+
+// Variable names are case-sensitive. This means that the message and Message are different variables.
+// Variable names can only contain letters, numbers, underscores, or dollar signs and cannot contain spaces. Also, variable names must begin with a letter, an underscore (_) or a dollar sign ($).
+// Variable names cannot use the reserved words.
+// By convention, variable names use camelcase like message, yourAge, and myName.
+
+// Three types of variables 
+// 1. Let - it can be reassigned because it is mutable, Let can be declared globally, but its access is limited to the block in which it is declared
+//  2. Var - Var can be declared and accessed globally.
+//   3. Const - Once you define a constant, you cannot change its value.
